429855f3cfe3fc4d588671608db0aa51bc5c6ffc
SKINGS_CR7 <surendrak00477@gmail.com>
2020-05-13 11:28:01 +0530

    Peacock Solar Project

---
+++ app/src/main/java/com/example/peacocksolarproject/SignUp_Form.java
@@ -0,0 +1,17 @@
+package com.example.peacocksolarproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SignUp_Form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up__form);
+
+    }
+}
